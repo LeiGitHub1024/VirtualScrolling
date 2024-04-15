f981515b91f32049df8b7124c01aa2660fcdefd4
mengxiangyu05 <mengxiangyu05@meituan.com>
2024-04-15 11:00:16 +0800

    feat: v1

---
+++ utils.js
@@ -0,0 +1,20 @@
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  }
+}
+
+function test() {
+  console.log('test');
+  
+}
+
+// export { throttle}
